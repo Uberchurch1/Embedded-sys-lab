00fc70ad1bf50d6d197a978740e32c334490b422
Uberchurch1 <64749080+Uberchurch1@users.noreply.github.com>
2025-03-03 15:49:11 -0500

    Create transcript_gui.py

---
+++ W7/Part2/test/transcript_gui.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import time
+from faster_whisperer import *
+root = Tk()#initializes a tk GUI
+
+recArray = ["Stop Recording", "Start Recording"]
+titleArray = ["Recording...", "Translator"]
+recVar = 1
+
+
+name_var = tk.StringVar()
+passw_var = tk.StringVar()
+name = None
+passw = None
+
+frm = ttk.Frame(root, padding=10)#sets the padding around the border of the window
+frm.grid()#sets the window to grid mode instead of place mode
+title = ttk.Label(frm, text=titleArray[recVar])#creates a text label
+title.grid(column=0, row=0)
+quitBut = ttk.Button(frm, text="Quit", command=root.destroy)#creates a clickable button that closes the window
+quitBut.grid(column=3, row=3)
+recBut = ttk.Button(frm, text="Start Recording")
+recBut.grid(column=2, row=3)
+
+subBut = ttk.Button(frm, text="Transcribe Audio")
+subBut.grid(column=2, row=2)
+
+
+def toggleRec():
+    global recVar, recArray, toggleBut, rec
+    if recVar == 1:
+        recVar = 0
+        start_recording()
+    else:
+        recVar = 1
+        stop_recording()
+    recBut.config(text=recArray[recVar])
+
+def transcribe():
+    top = Toplevel()
+    top.title("Transcription")
+    top.mainloop()
+    textScript = transcribe_audio()
+    transcript = ttk.label(top, text=textScript)
+
+
+subBut.config(command=transcribe)
+recBut.config(command=toggleRec)
+root.mainloop()#runs window program
+
