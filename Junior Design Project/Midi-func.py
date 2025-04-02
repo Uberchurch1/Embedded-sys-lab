ba7d6e3401b072d969ae08e5aead9a66a2780b16
Uberchurch1 <64749080+Uberchurch1@users.noreply.github.com>
2025-04-02 13:04:31 -0400

    Update Midi-func.py

--- Junior Design Project/Midi-func.py
+++ Junior Design Project/Midi-func.py
@@ -4,8 +4,13 @@ import rtmidi
 midiout= rtmidi.MidiOut()
 available_ports = midiout.get_ports()
 if available_ports:
-    midiout.open_port(0)
+    print(available_ports)
+    port = input("Choose a port number")
+    midiout.open_port(int(port))
+    print(f"Opened port {port}")
+#     midiout.open_port(1)
 else:
+    print("No port found")
     midiout.open_virtual_port("My virtual output")
 #sends a note of pitch Note to Channel (written as a hex value)
 def SendNote(Note: int, Velocity: int = 100, Channel: int = 0x00, Duration: int = 1) -> None:
